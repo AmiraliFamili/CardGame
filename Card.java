162d3119c7449827f8d5c7bc56ffb233891a93ac
Amirali <af689@exeter.ac.uk>
2023-11-04 18:15:11 +0000

    Worked on the parallel problems of the project and added some methods and changed the structure of Thread handling in the code.

--- Card.java
+++ Card.java
@@ -38,27 +38,109 @@ public class Card {
         return sHand;
     }
 
-    public int getCard(LinkedList<Integer> hand) {
-        Scanner scan = new Scanner(System.in);
-        String card = scan.nextLine();
+    public boolean hasDuplicates(LinkedList<Integer> hand) {
+        Set<Integer> seen = new HashSet<>();
 
-        try {
-            int num = Integer.parseInt(card);
-            for (Integer cards : hand) {
-                if (num == cards) {
-                    return num;
-                }
+        for (Integer card : hand) {
+            if (!seen.add(card)) {
+                return true;
             }
-            System.out.println("Please enter the card you wish to remove : " + hand);
-            System.out.println("Entered Value : " + num + "\n");
-            return getCard(hand);
-        } catch (NumberFormatException e) {
-            System.out.println("Please enter a valid Integer from hand : " + hand);
-            System.out.println("Entered Value : " + card + "\n");
-            return getCard(hand);
         }
+        return false;
     }
 
+    public int getCard(LinkedList<Integer> hand) { // this method will chose for the players what card should be
+                                                   // discarded
+        int card = 0;
+
+        if (hasDuplicates(hand)) {
+            Map<Integer, Integer> cardCount = new HashMap<>();
+            for (int c : hand) {
+                cardCount.put(c, cardCount.getOrDefault(c, 0) + 1);
+            }
+
+            for (int c : hand) {
+                if (cardCount.get(c) == 1) {
+                    System.out.println(c + " is not a duplicate in " + hand );
+                    card = c;
+                    break;
+                }
+            }
+        } else {
+            System.out.println(hand + " has no duplicates, returning the first element");
+            card = hand.getFirst();
+        }
+
+        return card;
+    }
+
+    private class PlayerThread extends Thread {
+        private LinkedList<Integer> player;
+        private LinkedList<Integer> insertTop;
+        private LinkedList<Integer> discardBottom;
+        int counter = 0;
+
+        public PlayerThread(LinkedList<Integer> player, LinkedList<Integer> insertTop,
+                LinkedList<Integer> discardBottom) {
+            this.player = player;
+            this.insertTop = insertTop;
+            this.discardBottom = discardBottom;
+        }
+
+        @Override
+        public void run() {
+            while (!win) {
+                playTurn();
+            }
+        }
+
+        private Boolean win = false;
+        private final Object lock = new Object();
+
+        public void playTurn() {
+            boolean hasPlayed = false;
+            
+            while (!hasPlayed) {
+                synchronized (lock) {
+                    while (insertTop.isEmpty()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+                    
+                    System.out.println("player_" + (counter + 1) % deckNumber + " remove one card from Hand: " + handToString(player));
+        
+                    System.out.println("1.player hand: " + player);
+                    System.out.println("1.discardBottom: " + discardBottom);
+                    System.out.println("1.insertTop: " + insertTop);
+        
+                    synchronized (players) {
+                        int card = getCard(player);
+                        player.remove( player.indexOf(getCard(player)));
+                        player.add(insertTop.removeFirst());
+                        discardBottom.add(card);
+                    }
+                    System.out.println("2.player hand: " + player);
+                    System.out.println("2.discardBottom: " + discardBottom);
+                    System.out.println("2.insertTop: " + insertTop);
+        
+                    if (player.size() >= 4 && player.get(0).equals(player.get(1)) && player.get(1).equals(player.get(2))
+                            && player.get(2).equals(player.get(3))) {
+                        this.win = true;
+                        System.out.println("Player" + (players.indexOf(player) + 1) + " wins!");
+                        break;
+                    }
+                    
+                    hasPlayed = true;
+                    counter++;
+                }
+            }
+        }
+    }
+        
     /*
      * maybe we can use left deck and right deck of the player to use multithreading
      * without any conflict
@@ -71,116 +153,24 @@ public class Card {
      * should we separate the players and decks ? how ?
      */
 
-    private Boolean win = false;
-/*
-
-public void startGame() {
-        ExecutorService executorService = Executors.newFixedThreadPool(players.size());
-        executorService.execute(() -> {
-            while (!win) {
-                for (int playerIndex = 0; playerIndex < players.size(); playerIndex++) {
-                    // Make sure to use synchronization mechanisms to coordinate turns
-                    synchronized (players) { // now that it's in synchronized is it running in parallel ?
-                        LinkedList<Integer> player = this.players.get(playerIndex % deckNumber);
-                        LinkedList<Integer> insertTop = this.decks.get(playerIndex % deckNumber);
-                        LinkedList<Integer> discardBottom = this.decks.get((playerIndex + 1) % deckNumber);
-
-                        System.out.println(
-                                "player_" + (playerIndex + 1) % deckNumber + " remove one card from Hand : "
-                                        + handToString(player) + "\n");
-                        int Card = getCard(player);
-                        System.out.println("1.Card : " + Card);
-                        System.out.println("1.player hand : " + player);
-                        System.out.println("1.discardBottom : " + discardBottom);
-                        System.out.println("1.insertTop : " + insertTop);
-
-                        int index = player.indexOf(Card);
-
-                        player.remove(index);
-                        synchronized (decks) {
-                            // with this block all actions to decks are monitored and happen one at a time
-                            player.add(insertTop.removeFirst());
-                            discardBottom.add(Card);
-                        }
-                        // we should take the same approach with players list
-                        System.out.println("2.player hand :  " + player);
-                        System.out.println("2.discardBottom : " + discardBottom);
-                        System.out.println("2.insertTop : " + insertTop);
-
-                        // round robin
-                        // Threads
-
-                        // writing to files should happen in an synchronized block for each player
-
-                        if (player.get(0) == player.get(1) && player.get(1) == player.get(2)
-                                && player.get(2) == player.get(3)) {
-                            win = true;
-                            System.out.println("player " + (playerIndex + 1) % deckNumber + " Wins !");
-                            // notifyAll(); this doesn't work but we have to notify the other players that
-                            // player n has won the game
-                        }
-                    }
-
-                }
-            }
-        });
-
-        executorService.shutdown();
-    }
-    */
-
-
-    public void startGame() { // this code will show that how the players are running in parallel 
-        // if we can update the code so that it performs correctly 
-        // mainly to understand how can we communicate with the game in way that it's parallel 
+    public void startGame() { // this code will show that how the players are running in parallel
+        // if we can update the code so that it performs correctly
+        // mainly to understand how can we communicate with the game in way that it's
+        // parallel
         ExecutorService executorService = Executors.newFixedThreadPool(players.size());
 
-        for (int playerIndex = 0; playerIndex < players.size(); playerIndex++) {
-            final int currentIndex = playerIndex;
+        for (int i = 0; i < players.size(); i++) {
+            LinkedList<Integer> player = players.get(i);
+            LinkedList<Integer> insertTop = decks.get(i);
+            LinkedList<Integer> discardBottom = decks.get((i + 1) % deckNumber);
 
-            executorService.execute(() -> {
-                while (!win) {
-                    LinkedList<Integer> player = players.get(currentIndex % deckNumber);
-                    LinkedList<Integer> insertTop = decks.get(currentIndex % deckNumber);
-                    LinkedList<Integer> discardBottom = decks.get((currentIndex + 1) % deckNumber);
-
-                    System.out.println(
-                            "player_" + (currentIndex + 1) % deckNumber + " remove one card from Hand: "
-                                    + handToString(player) + "\n");
-                    int card = getCard(player);
-                    System.out.println("1.Card: " + card);
-                    System.out.println("1.player hand: " + player);
-                    System.out.println("1.discardBottom: " + discardBottom);
-                    System.out.println("1.insertTop: " + insertTop);
-
-                    int index = player.indexOf(card);
-                    
-
-                    synchronized (decks) {
-                        player.remove(index);
-                        player.add(insertTop.removeFirst());
-                        discardBottom.add(card);
-                    }
-
-                    System.out.println("2.player hand: " + player);
-                    System.out.println("2.discardBottom: " + discardBottom);
-                    System.out.println("2.insertTop: " + insertTop);
-
-                    if (player.size() >= 4 && player.get(0).equals(player.get(1)) && player.get(1).equals(player.get(2))
-                            && player.get(2).equals(player.get(3))) {
-                        win = true;
-                        System.out.println("Player " + (currentIndex + 1) % deckNumber + " Wins!");
-                    }
-                }
-            });
+            PlayerThread playerThread = new PlayerThread(player, insertTop, discardBottom);
+            executorService.execute(playerThread);
+            
         }
 
         executorService.shutdown();
     }
-
-
-
-    
 
     public void playerWon() {
 
@@ -221,3 +211,65 @@ public void startGame() {
      * 
      */
 }
+
+/*
+ * 
+ * public void startGame() {
+ * ExecutorService executorService =
+ * Executors.newFixedThreadPool(players.size());
+ * executorService.execute(() -> {
+ * while (!win) {
+ * for (int playerIndex = 0; playerIndex < players.size(); playerIndex++) {
+ * // Make sure to use synchronization mechanisms to coordinate turns
+ * synchronized (players) { // now that it's in synchronized is it running in
+ * parallel ?
+ * LinkedList<Integer> player = this.players.get(playerIndex % deckNumber);
+ * LinkedList<Integer> insertTop = this.decks.get(playerIndex % deckNumber);
+ * LinkedList<Integer> discardBottom = this.decks.get((playerIndex + 1) %
+ * deckNumber);
+ * 
+ * System.out.println(
+ * "player_" + (playerIndex + 1) % deckNumber + " remove one card from Hand : "
+ * + handToString(player) + "\n");
+ * int Card = getCard(player);
+ * System.out.println("1.Card : " + Card);
+ * System.out.println("1.player hand : " + player);
+ * System.out.println("1.discardBottom : " + discardBottom);
+ * System.out.println("1.insertTop : " + insertTop);
+ * 
+ * int index = player.indexOf(Card);
+ * 
+ * player.remove(index);
+ * synchronized (decks) {
+ * // with this block all actions to decks are monitored and happen one at a
+ * time
+ * player.add(insertTop.removeFirst());
+ * discardBottom.add(Card);
+ * }
+ * // we should take the same approach with players list
+ * System.out.println("2.player hand :  " + player);
+ * System.out.println("2.discardBottom : " + discardBottom);
+ * System.out.println("2.insertTop : " + insertTop);
+ * 
+ * // round robin
+ * // Threads
+ * 
+ * // writing to files should happen in an synchronized block for each player
+ * 
+ * if (player.get(0) == player.get(1) && player.get(1) == player.get(2)
+ * && player.get(2) == player.get(3)) {
+ * win = true;
+ * System.out.println("player " + (playerIndex + 1) % deckNumber + " Wins !");
+ * // notifyAll(); this doesn't work but we have to notify the other players
+ * that
+ * // player n has won the game
+ * }
+ * }
+ * 
+ * }
+ * }
+ * });
+ * 
+ * executorService.shutdown();
+ * }
+ */
