20359ded9610e8bc9f74a8bbcd41fb1d312c3254
kaylum1 <kms222@exeter.ac.uk>
2023-11-06 10:08:58 +0000

    i have just added the work that was done last week to the githib repository

--- Player.java
+++ Player.java
@@ -1,19 +1,109 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Random;
+import java.io.BufferedWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Player {
 
     final private int playerNumber;
+    static ArrayList<Player> players = new ArrayList<>();
+    boolean firstT;
+
     
-    /*
-     * has n players
-     */
+    //to get player number
+    
     public Player(int playerNumber){
         this.playerNumber = playerNumber;
+        players.add(this);
+        this.firstT = true;
+    }
+
+    //more work needs to be done between line 23-50
+    public void initalHand(int playerNumber) {
+        return dealHands
+    }
+    
+
+     public void getHands(int playerNumber) {
+
+    }
+
+    public void drawCard(int playerNumber) {
+        this.cardDrawn = cardDrawn;
+        // Implementation here need to be added so code undertsand that a player has drawn card to listen to card.java
+    }
+ 
+    public void discardCard(int cardDiscarded) {
+       // same here i dont unsetrdan how be simple code need to be added to it knows when a player has discarded a cad from the
+       //main card game
+    }
+
+   
+ 
+    public boolean hasWon() {
+        return false;
+        // Implementation here
+    }
+
+    
+
+//this section is where the code will use to create logs for output/logs folder
+    public void logAction(String action) {
+        try {
+            String directoryPath = "output/logs/";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(directoryPath + "player" + playerNumber + "_output.txt", true));
+            writer.append(action);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /* 
+        try {
+            String directoryPath = "output/logs";
+            File directory = new File(directoryPath);
+            
+            BufferedWriter writer = new BufferedWriter(new FileWriter(directoryPath + "player" + playerNumber + "_output.txt", true));
+            writer.append(action);
+            writer.newLine();
+            writer.close();
+            } catch (IOException e) {
+            e.printStackTrace();
+            }
+*/
+        
+    }
+
+    public void startGame() {
+        //start of the game requires this (got the "dealHands" from cardGame.java)
+        logAction("Player " + playerNumber + " innital hand is " + dealHands);
+
+        //this logs the what cards player has drawn (needs to find correct function name fpr cardDrawn)
+        logAction("Player " + playerNumber + " has drawn a " + cardDrawn);
+        //this logs the what cards player discarded (got "getCard" from card.java)
+        logAction("Player " + playerNumber + " has discarded a " + getCard);
+        //this logs the what cards player currently has (got "getHands" form cardGAme.java)
+        logAction("Player " + playerNumber + "'s is " + getHands);
+        //action taken when there is a winner
+        if (hasWon()){
+            logAction("Player " + playerNumber + " wins");
+            logAction("Player" + playerNumber + " exits");
+            logAction("Player " + playerNumber + " final hand " + hand);
+            }
     }
 
 
+
 }
+
+
+
+/*
+ * EXTRA NOTES FOR IMPLMENTAION 
+ * implements the Callable interface, which is part of Java's concurrency API and is often used in conjunction with threads.
+    interface similar to Runnable interface, but it allows for a return value and can throw exceptions. 
+    In the Player class, the call() method is implemented, which is the method that will be called when a Player object is used with an executor service, 
+    such as ExecutorService.submit(Callable<T> task)
+ */
